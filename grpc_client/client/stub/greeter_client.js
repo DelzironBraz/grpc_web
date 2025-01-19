018b05fa4341c3b4df3af14d9ef647679bb262a2
LamequeFernandes <lamequesao@gmail.com>
2025-01-19 01:21:33 -0300

    feat(possibilitando main a ser chamada)

--- grpc_client/client/stub/greeter_client.js
+++ grpc_client/client/stub/greeter_client.js
@@ -3,6 +3,7 @@ var PROTO_PATH = __dirname + "/../../protos/helloworld.proto";
 var parseArgs = require("minimist");
 var grpc = require("@grpc/grpc-js");
 var protoLoader = require("@grpc/proto-loader");
+
 var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   keepCase: true,
   longs: String,
@@ -12,8 +13,8 @@ var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
 });
 var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
 
-function main() {
-  var argv = parseArgs(process.argv.slice(2), {
+function main(args = null) {
+  var argv = parseArgs(args ? args[0] : process.argv.slice(2), {
     string: ["target", "mode"],
     alias: { t: "target", m: "mode" },
   });
@@ -29,29 +30,40 @@ function main() {
   if (mode === "inclusao") {
     if (argv._.length === 0) {
       console.error("Erro: Nenhuma palavra fornecida para inclusão.");
-      return;
+      return 'Nenhuma palavra fornecida para inclusão';
     }
 
     argv._.forEach((word) => {
       client.sayHello({ name: word }, function (err, response) {
         if (err) {
           console.error("Erro ao enviar palavra:", err.message);
+          return `Erro ao enviar palavra: ${err.message}`;
         } else {
           console.log("Resposta do servidor:", response.message);
+          return response.message;
         }
       });
     });
   } else if (mode === "consulta") {
-    client.sayHello({ name: "IMPRIMIR" }, function (err, response) {
-      if (err) {
-        console.error("Erro ao consultar dicionário:", err.message);
-      } else {
-        console.log("Dicionário do servidor:\n", response.message);
-      }
+    return new Promise((resolve, reject) => {
+      client.sayHello({ name: "IMPRIMIR" }, function (err, response) {
+        if (err) {
+          console.error("Erro ao consultar dicionário:", err.message);
+          reject(`Erro ao consultar dicionário: ${err.message}`);
+        } else {
+          console.log("Dicionário do servidor:\n", response.message);
+          resolve(response.message);
+        }
+      });
     });
   } else {
     console.error("Erro: Modo inválido. Use 'inclusao' ou 'consulta'.");
+    return 'Modo inválido. Use "inclusao" ou "consulta".';
   }
 }
 
 main();
+
+module.exports = {
+  main
+};
